43db001f796eae91a3e68441edeaf6fba64ce1f1
Girish Joshi <girish.joshi203@gmail.com>
2024-03-24 00:59:25 -0400

    added module config to  main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,18 @@ resource "azurerm_resource_group" "c2c-rg" {
 module "vnet" {
   source = "./modules/vnet"
   # Add any required variables here
-  
+  vnet_name           = var.vnet_name
+  location            = azurerm_resource_group.resource_group_name.location
+  resource_group_name = azurerm_resource_group.resource_group_name.name
+  address_space       = var.address_space
 
 }
 
 module "subnet" {
   source = "./modules/subnet"
   # Add any required variables here
+  vnet_name            = var.vnet_name
+  subnet_name          = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  address_prefixes     = var.address_prefixes
 }
