44b18d3391c653b7deee67c47974e018d40d98af
Girish Joshi <girish.joshi203@gmail.com>
2024-03-24 05:28:45 -0400

    modify for each logic to fix issue with west us

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "c2c-rg" {
-  for_each = { for region in var.regions : region => region }
+  for_each = { for region, region in var.regions : region => region }
   name     = "C2C-RG-${each.value}"
   location = "${each.value} us"
 }
