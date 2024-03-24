ccba42904c92e07d46a61cd079f9732c107db70c
Girish Joshi <girish.joshi203@gmail.com>
2024-03-24 00:38:34 -0400

    add vnet and subnet variables to main variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,54 @@ variable "subnet_name" {
   type    = string
   default = "example-subnet"
 }
+
+#Variables from the vnet module
+variable "resource_group_name" {
+  type    = string
+  description = "The name of the Azure resource group where the VNet will be created."
+}
+
+variable "location" {
+  type    = string
+  description = "The Azure region where the VNet will be created."
+}
+
+variable "vnet_name" {
+  type    = string
+  description = "The name of the Azure VNet."
+}
+
+variable "address_space" {
+  type    = list(string)
+  description = "The address space for the VNet in CIDR notation. Example: ['10.0.0.0/16']"
+}
+
+variable "tags" {
+  type    = map(string)
+  description = "A map of tags to assign to the VNet."
+  default = {}
+}
+
+
+#Variables from the subnet module
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the Azure resource group where the subnet will be created."
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "The name of the Azure VNet where the subnet will be created."
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "The name of the Azure subnet."
+}
+
+variable "address_prefixes" {
+  type        = list(string)
+  description = "The address prefixes for the subnet in CIDR notation. Example: ['10.0.1.0/24']"
+}
+
+
