c06eb14034270244a8e3b1a60a8b4a05dbcbbdb6
Girish Joshi <girish.joshi203@gmail.com>
2024-03-23 21:04:00 -0400

    single region resource group

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 resource "azurerm_resource_group" "c2c-rg" {
-  for_each = { for loc, region in var.region : loc => region }
-
-  name     = "C2C-rg-${each.value}"
-  location = each.value
+  name     = "C2C-rg"
+  location = "east us"
 }
